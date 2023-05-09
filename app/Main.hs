5f2dd786fee4270199acdd2882aa370b0c86dceb
Xuan Tam Trinh <trinh@post.cz>
2023-05-09 23:07:40 +0200

    add CLI parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
+import Options.Applicative
+
+-- A data type to hold the command line options
+data Options = Options
+  { sourcePath :: FilePath
+  }
+
+-- A parser for the command line options
+optionsParser :: Parser Options
+optionsParser = Options
+  <$> strOption
+      ( long "src"
+     <> metavar "PATH"
+     <> help "The path to the source image file" )
+
 main :: IO ()
-main = undefined
+main = do
+  options <- execParser $ info (optionsParser <**> helper)
+    ( fullDesc
+   <> progDesc "A command-line utility that takes an image from a provided input path, converts the image into ASCII art, and optionally applies filters."
+   <> header "ASCII Art Generator" )
